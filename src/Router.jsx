12e6dbce0a37c8697054646d241b741a99e9137d
Carlos Pires <extraterra1@gmail.com>
2024-01-04 11:23:49 +0000

    dev: react router set up

---
+++ src/Router.jsx
@@ -0,0 +1,15 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Landing from './components/Landing';
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Landing />
+    }
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
---
+++ src/components/Landing.jsx
@@ -0,0 +1,5 @@
+const Landing = () => {
+  return <h1>xdss</h1>;
+};
+
+export default Landing;
