4cae9b82bfdb8ed4df85481c54560a2ade71aa23
Carlos Pires <extraterra1@gmail.com>
2024-01-08 14:06:56 +0000

    dev: authProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './Router';
+import Router from './router';
 import WebFont from 'webfontloader';
+import createStore from 'react-auth-kit/createStore';
+import AuthProvider from 'react-auth-kit/AuthProvider';
 import './index.css';
+
+const store = createStore({
+  authName: '_auth',
+  authType: 'cookie',
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === 'https:'
+});
 
 WebFont.load({
   google: {
@@ -12,6 +21,8 @@ WebFont.load({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <AuthProvider store={store}>
+      <Router />
+    </AuthProvider>
   </React.StrictMode>
 );
