81e5f031cc918d126f570f0c4bd52d0a7522e0d4
Carlos Pires <extraterra1@gmail.com>
2024-01-09 14:59:51 +0000

    dev: redirect if user is logged

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Landing from './components/Landing';
 import Login from './components/Login';
+import Register from './components/Register';
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -12,6 +13,10 @@ const Router = () => {
     {
       path: '/login',
       element: <Login />
+    },
+    {
+      path: '/register',
+      element: <Register />
     }
   ]);
   return <RouterProvider router={router} />;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,13 @@
+import { Navigate } from 'react-router-dom';
+import useIsAuthenticated from 'react-auth-kit/hooks/useIsAuthenticated';
+
 import Header from './Header';
 
 const Register = () => {
+  const isAuthenticated = useIsAuthenticated();
   return (
     <>
+      {isAuthenticated() ? <Navigate to="/" /> : null}
       <Header />
       <main />
     </>
