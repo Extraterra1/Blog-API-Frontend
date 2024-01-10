d72f229be6f56502330f10e8388d516371c23c96
Carlos Pires <extraterra1@gmail.com>
2024-01-10 14:37:57 +0000

    dev: initial userDashboard

---
+++ src/views/UserDetail.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+
+const UserDashboard = () => {
+  return (
+    <>
+      <Header />
+      <main />
+    </>
+  );
+};
+
+export default UserDashboard;
