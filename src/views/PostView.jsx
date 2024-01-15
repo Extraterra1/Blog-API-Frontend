bff9be3b610dfef1dab03db88d25fb2b68b02399
Carlos Pires <extraterra1@gmail.com>
2024-01-15 14:12:52 +0000

    dev: fetch post if valid id

--- src/views/PostView.jsx
+++ src/views/PostView.jsx
@@ -1,3 +1,36 @@
-const PostView = () => {};
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import useAxios from 'axios-hooks';
+
+import Header from '../components/Header';
+
+const validatePostId = (value) => {
+  const regex = /^[a-fA-F0-9]{24}$/;
+
+  if (value !== '' && typeof value === 'string') {
+    const result = value.match(regex);
+    return result?.length > 0;
+  }
+  return false;
+};
+
+const PostView = () => {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const [{ data, loading, error }, fetchPost] = useAxios({ url: `${import.meta.env.VITE_API_URL}/posts/${params.id}`, method: 'GET' }, { manual: false });
+  console.log(data);
+
+  useEffect(() => {
+    if (!validatePostId(params.id)) return navigate('/');
+  });
+
+  return (
+    <>
+      <Header />
+      <main />
+    </>
+  );
+};
 
 export default PostView;
