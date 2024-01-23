41a0c19292959a7f287ae176c758fa739601f8ae
Carlos Pires <extraterra1@gmail.com>
2024-01-23 15:30:29 +0000

    dev: display comment count

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,7 +1,25 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Container = styled.div`
+  /* margin-top: 0rem; */
+`;
+
 const Comments = ({ comments }) => {
-  return comments.map((el) => {
-    return <div key={el._id}>{el.content}</div>;
-  });
+  return (
+    <>
+      <hr style={{ margin: '10rem 0' }} />
+      <Container>
+        <div className="title">
+          <h2>{comments.length === 0 ? 'No comments yet, be the first!' : comments.length === 1 ? '1 Comment' : `${comments.length} Comments`}</h2>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+Comments.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default Comments;
