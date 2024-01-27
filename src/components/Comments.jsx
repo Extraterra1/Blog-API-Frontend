b44404e411b9214714b54b75405e78a4477a3b84
Carlos Pires <extraterra1@gmail.com>
2024-01-27 12:15:11 +0000

    feat: show link to login in comments when not logged in

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { useIsAuthenticated } from 'react-auth-kit';
 import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import InsertCommentForm from './InsertCommentForm';
 import Comment from './Comment';
@@ -13,10 +14,19 @@ const Container = styled.div`
   gap: 2rem;
 `;
 
+const StyledLink = styled(Link)`
+  & span {
+    color: var(--info);
+    text-decoration: underline;
+    text-underline-offset: 0.5rem;
+  }
+`;
+
 const Comments = ({ commentsArr }) => {
   const isAuthenticated = useIsAuthenticated();
   const [comments, setComments] = useState(commentsArr);
   const [modal, setModal] = useState({ open: false, comment: {} });
+  const { pathname } = useLocation();
 
   const closeModal = () => setModal({ ...modal, open: false });
 
@@ -27,7 +37,13 @@ const Comments = ({ commentsArr }) => {
         <div className="title">
           <h2>{comments.length === 0 ? 'No comments yet, be the first!' : comments.length === 1 ? '1 Comment' : `${comments.length} Comments`}</h2>
         </div>
-        {isAuthenticated() ? <InsertCommentForm setComments={setComments} /> : null}
+        {isAuthenticated() ? (
+          <InsertCommentForm setComments={setComments} />
+        ) : (
+          <StyledLink to="/login" state={{ pathname }}>
+            You need to be <span>logged in</span> to leave a comment.
+          </StyledLink>
+        )}
         {comments.map((e) => (
           <Comment key={e._id} comment={e} setModal={setModal} />
         ))}
